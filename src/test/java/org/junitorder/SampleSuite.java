819f3c72331dd11b586a496cf894349f7099d1fd
Ejaz1818 <ejaznive786@gmail.com>
2021-12-01 23:12:07 -0500

    I add samplesuite

--- src/test/java/org/junitorder/SampleSuite.java
+++ src/test/java/org/junitorder/SampleSuite.java
@@ -1,5 +1,31 @@
 package org.junitorder;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
 
 public class SampleSuite {
+	@Test
+	public void tc1() {
+		System.out.println("test 1");
+		
+	}
+	@Test
+	public void tc2() {
+		System.out.println("test 2");
+		
+	}
+	@Ignore
+	@Test
+	public void tc3() {
+		System.out.println("test 3");
+		
+	}
+	@Test
+	public void tc4() {
+		Assert.assertTrue(false);
+		System.out.println("test 4");
+		
+	}
+	
 
 }
